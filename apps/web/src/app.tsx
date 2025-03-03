53839367a0c5a06dd078b44412e452f0a479510e
Yogendra Rana <yogendrarana.tech@gmail.com>
2025-03-03 21:36:13 +0545

    added vercel analytics

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router";
 import Index from "./components/pages";
+import { Analytics } from "@vercel/analytics/react"
 import NotFound from "./components/pages/not-found";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
     return (
         <BrowserRouter>
+            <Analytics /> 
             <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="*" element={<NotFound />} />
