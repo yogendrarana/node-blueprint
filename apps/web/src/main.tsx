45fc7fedf22b6ddc8322b048998c8dbeaf1887e9
Yogendra Rana <yogendrarana.tech@gmail.com>
2025-03-31 22:40:14 +0545

    remove redundant posthog initialization

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,9 @@
-import posthog from "posthog-js";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./styles/main.css";
 import App from "./app.tsx";
 import Providers from "./components/providers.tsx";
-
-posthog.init(import.meta.env.VITE_POSTHOG_KEY, { api_host: import.meta.env.VITE_POSTHOG_HOST });
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
